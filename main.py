715afcf54e054c19592703a4842a17bc08fdaee7
Loic Dogon <loic.dogon@gmail.com>
2025-06-12 12:12:29 +0200

    t

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import os
 from api.fast import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8001))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("api.fast:app", host="0.0.0.0", port=port, reload=True)
